cf4367627c9f2199336bffaab729b5491fb919d4
Sublime <other@Alberts-MBP.fios-router.home>
2018-09-27 22:46:45 -0400

    finish 0a and 0b

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,28 +1,41 @@
+  public class ArrayDemo{
 
-public class ArrayDemo{
-  public static void printArray(int[]ary){
-    String var = "";
-    for (int x = 0; x < ary.length; x ++){
-      if (x != ary.length - 1){
-        var += ary[x] + ", ";
-      }else{
-        var += ary[x];
+    public static void printArray(int[][]ary){
+      int counter = 0;
+      for (int x = 0; x < ary.length; x++){
+        for (int y = 0; y < ary[x].length; y++){
+            System.out.print(ary[x][y] + " ");
+        }
+        System.out.println();
       }
     }
-    return var;
-  }
-  public static void printArray(int[][]ary){
-    String var = "";
-    for (int x = 0; x < ary.length; x ++){
-      for (int y = 0; y < ary[].length; y++){
+
+
+
+
+    public static void printArray(int[]ary){
+      for (int x = 0; x < ary.length; x ++){
         if (x != ary.length - 1){
-          var += ary[x][y] + ", ";
+          System.out.print(ary[x] + ", ");
         }else{
-          var += ary[x][y];
+          System.out.print(ary[x]);
         }
       }
-
     }
-    return var;
-  }
+
+    public static void main(String[] args){
+
+      int[] array = {
+        3,4,5,67,8,9,9,3
+      };
+
+      int[][] ar = {
+        {1,23,4,6,6},
+        {3,4,56,7,3}
+      };
+      printArray(array);
+      System.out.println();
+      System.out.println();
+      printArray(ar);
+      }
 }
