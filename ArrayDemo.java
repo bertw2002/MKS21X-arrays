8c1ec75d24b5742351fb9e337430547affaa9fbd
Sublime <other@Alberts-MBP.fios-router.home>
2018-09-27 20:14:39 -0400

    made code

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -10,4 +10,18 @@ public class ArrayDemo{
     }
     return var;
   }
+  public static void printArray(int[][]ary){
+    String var = "";
+    for (int x = 0; x < ary[].length; x ++){
+      for (int y = 0; y < ary[][].length; y++){
+        if (x != ary.length - 1){
+          var += ary[x][y] + ", ";
+        }else{
+          var += ary[x][y];
+        }
+      }
+
+    }
+    return var;
+  }
 }
